e7dd5124a92c5825aa75ca728de6374a26902ce1
Gaby G <paogaby0803@gmail.com>
2020-09-21 00:16:24 -0500

    example password verifier

---
+++ examples/password_verifier/password_verifier.js
@@ -0,0 +1,14 @@
+const log = require('./complicated-logger');
+
+const verifyPassword = (input, rules) => {
+    const failed = rules.map(rule => rule(input))
+    .filter(result => result === false);
+    if (failed.count === 0) {
+        log.info('PASSED');
+        return true;
+    }
+
+    log.info('FAIL');
+    return false;
+}    
+
